4aff8d77ae44ed553ec5b8332017aa07aa6f5ae6
kareniel <jonathan.dupre@gmail.com>
2017-03-30 15:06:01 -0400

    init

---
+++ data/dummy.js
@@ -0,0 +1,57 @@
+module.exports.directories = [
+  {
+    "name": "2016",
+    "path": "2016",
+    "url": "https://api.github.com/repos/kareniel/diary-content/contents/2016?ref=master",
+    "type": "dir",
+    "files": [
+      {
+        "name": "2016-08-18.md",
+        "path": "2016/2016-08-18.md",
+        "url": "https://api.github.com/repos/kareniel/diary-content/contents/2016/2016-08-18.md?ref=master",
+        "type": "file"
+      },
+      {
+        "name": "2016-08-19.md",
+        "path": "2016/2016-08-19.md",
+        "url": "https://api.github.com/repos/kareniel/diary-content/contents/2016/2016-08-19.md?ref=master",
+        "type": "file"
+      },
+      {
+        "name": "2016-08-21.md",
+        "path": "2016/2016-08-21.md",
+        "url": "https://api.github.com/repos/kareniel/diary-content/contents/2016/2016-08-21.md?ref=master",
+        "type": "file"
+      }
+    ]
+  },
+  {
+    "name": "2017",
+    "path": "2017",
+    "url": "https://api.github.com/repos/kareniel/diary-content/contents/2017?ref=master",
+    "type": "dir",
+    "files": [
+      {
+        "name": "2017-01-24.md",
+        "path": "2017/2017-01-24.md",
+        "url": "https://api.github.com/repos/kareniel/diary-content/contents/2017/2017-01-24.md?ref=master",
+        "type": "file"
+      },
+      {
+        "name": "2017-01-25.md",
+        "path": "2017/2017-01-25.md",
+        "url": "https://api.github.com/repos/kareniel/diary-content/contents/2017/2017-01-25.md?ref=master",
+        "type": "file"
+      },
+      {
+        "name": "2017-02-02.md",
+        "path": "2017/2017-02-02.md",
+        "url": "https://api.github.com/repos/kareniel/diary-content/contents/2017/2017-02-02.md?ref=master",
+        "type": "file"
+      }
+    ]
+
+  }
+]
+
+
---
+++ script.js
@@ -0,0 +1,6 @@
+const yo = require('yo-yo')
+const {directories} = require('./data/dummy')
+
+module.exports = function init (config) {
+
+}
